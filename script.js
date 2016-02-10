f35385ba56af840263957f345a6191063b667ecf
saleha123 <salehalinta@yahoo.com>
2016-02-10 23:27:46 +0000

    example comment

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){ 
+    
+    $('button').click( function() {
+        
+        $('body').css("background-color", color[i] i = 0, i > 6, i++);
+       
+        
+    });
+});
+ 
+
+var color = ["green", "blue", "red", "pink", "purple", "yellow"];
